dae293d8d1330f79c52a6577495e50161a7b611a
Harrison Massey <harrison.massey@gmail.com>
2012-10-02 13:24:58 -0500

    Actually used yoDawgTemplate

--- server/server.js
+++ server/server.js
@@ -8,10 +8,6 @@ var yoDawgTemplate = '<iframe width="{width}" height="{height}" href="{url}" />'
 //Init
 var app = express();
 
-app.get('/', function(req, res){
-    res.send(template.replace("{width}", req.query.width).replace("{height}", req.query.height));
-});
-
 app.get('/yodawg', function(req, res){
     var url = req.query.location;
     var queryPos = url.indexOf("?");
@@ -24,7 +20,11 @@ app.get('/yodawg', function(req, res){
         url = url.substr(0, queryPos + 1) + "arnoreplace=yes&" + url.substr(queryPos + 1, url.length - (queryPos + 1));
     }
 
-    res.send(template.replace("{width}", req.query.width).replace("{height}", req.query.height).replace("{url}", req.query.url));
+    res.send(yoDawgTemplate.replace("{width}", req.query.width).replace("{height}", req.query.height).replace("{url}", req.query.url));
+});
+
+app.get('/', function(req, res){
+    res.send(template.replace("{width}", req.query.width).replace("{height}", req.query.height));
 });
 
 app.listen(3000);
