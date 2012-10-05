76982363fcb64f8b86888d446c24fd4f68a6c39b
Max <max@server>
2012-10-05 10:47:15 -0500

    Added route to pull randomly from Reddit's "hot" list.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 //Requires
-var express = require('express');
+var express = require('express'),
+    request = require('request'),
+    _ = require('underscore');
+
+;
 
 //Constants
 var template = '<img src="http://placekitten.com/{width}/{height}" />'
+
 var yoDawgTemplate = '<iframe width="{width}" height="{height}" src="{url}" />'
+
+// var redditTemplate = '<iframe width="{width}" height="{height}" src="about:blank"><head /><body><a href"{url}">{title}</a><br><br> <a href="http://reddit.com{permalink}">comments</a></body></iframe>'
+
 
 //Init
 var app = express();
@@ -25,6 +33,54 @@ app.get('/yodawg', function(req, res){
 
 app.get('/', function(req, res){
     res.send(template.replace("{width}", req.query.width).replace("{height}", req.query.height));
+});
+
+
+//Reddit serverin'
+
+var options = {
+  url: 'http://www.reddit.com/hot.json',
+  json: true
+};
+
+var num;
+var redditRequest = {title: 'error', url: 'error', permalink: 'error'};
+
+function getTop() {
+    
+    request(options, function(error, res, obj) {
+
+        console.log("calling reddit...");
+
+        if (!error && res.statusCode == 200) {
+
+            num = _.random(0 ,obj['data']['children'].length -1 )
+            redditRequest.title = obj['data']['children'][num]['data']['title'];
+            redditRequest.permalink = obj['data']['children'][num]['data']['permalink'];
+            redditRequest.url = obj['data']['children'][num]['data']['url'];
+
+        }
+
+        else if (error) {
+
+            console.log("Problems connecting to Reddit.");
+        
+        }
+
+    });
+
+}
+
+
+app.get('/reddit', function(req, res) {
+
+    getTop();
+
+    res.send("<a href=\"" + redditRequest.url + "\">" + redditRequest.title + 
+         "</a> <br><br> <a href=\"http://reddit.com" + redditRequest.permalink + "\">" + "comments</a>");
+
+    // res.send(redditTemplate.replace("{width}", req.query.width).replace("{height}", req.query.height).replace("{url}", topPost.url).replace("{title}", topPost.title).replace("{permalink}", topPost.permalink));
+
 });
 
 app.listen(3000);
